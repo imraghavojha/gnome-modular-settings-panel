ad13d9fe1436dd75b2002aaf11649b1b95851f55
raghav <vindiesel38000@gmail.com>
2025-12-14 18:00:00 -0600

    refactor: Basic structure for extension and prefs

--- extension.js
+++ extension.js
@@ -2,36 +2,64 @@
 
 const { St, Gio } = imports.gi;
 const Main = imports.ui.main;
+const ExtensionUtils = imports.misc.extensionUtils;
 
 class Extension {
     constructor(uuid) {
         this._uuid = uuid;
+        this._settings = ExtensionUtils.getSettings(
+            'org.gnome.shell.extensions.modular-settings-panel');
     }
 
     enable() {
-        this._indicator = new St.Bin({
-            style_class: 'panel-button',
-            reactive: true,
-            can_focus: true,
-            track_hover: true
+        this._indicator = null;
+
+        this._settings.connect('changed::show-indicator', () => {
+            if (this._settings.get_boolean('show-indicator')) {
+                this._showIndicator();
+            } else {
+                this._hideIndicator();
+            }
         });
 
-        let icon = new St.Icon({
-            icon_name: 'system-run-symbolic',
-            style_class: 'system-status-icon'
-        });
-
-        this._indicator.set_child(icon);
-        this._indicator.connect('button-press-event', () => {
-            Main.notify('Hello World!');
-        });
-
-        Main.panel.addToStatusArea(this._uuid, this._indicator);
+        if (this._settings.get_boolean('show-indicator')) {
+            this._showIndicator();
+        }
     }
 
     disable() {
-        this._indicator.destroy();
-        this._indicator = null;
+        this._hideIndicator();
+        this._settings = null;
+    }
+
+    _showIndicator() {
+        if (!this._indicator) {
+            this._indicator = new St.Bin({
+                style_class: 'panel-button',
+                reactive: true,
+                can_focus: true,
+                track_hover: true
+            });
+
+            let icon = new St.Icon({
+                icon_name: 'system-run-symbolic',
+                style_class: 'system-status-icon'
+            });
+
+            this._indicator.set_child(icon);
+            this._indicator.connect('button-press-event', () => {
+                Main.notify('Hello World!');
+            });
+
+            Main.panel.addToStatusArea(this._uuid, this._indicator);
+        }
+    }
+
+    _hideIndicator() {
+        if (this._indicator) {
+            this._indicator.destroy();
+            this._indicator = null;
+        }
     }
 }
 
--- prefs.js
+++ prefs.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const { Adw, Gio, Gtk } = imports.gi;
+const ExtensionUtils = imports.misc.extensionUtils;
 
 function init() {
 }
 
 function fillPreferencesWindow(window) {
+    const settings = ExtensionUtils.getSettings(
+        'org.gnome.shell.extensions.modular-settings-panel');
+
     const page = new Adw.PreferencesPage();
     const group = new Adw.PreferencesGroup({
         title: 'General',
@@ -18,9 +22,16 @@ function fillPreferencesWindow(window) {
     group.add(row);
 
     const toggle = new Gtk.Switch({
-        active: true,
+        active: settings.get_boolean('show-indicator'),
         valign: Gtk.Align.CENTER,
     });
+    settings.bind(
+        'show-indicator',
+        toggle,
+        'active',
+        Gio.SettingsBindFlags.DEFAULT
+    );
+
     row.add_suffix(toggle);
 
     window.add(page);
