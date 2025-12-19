3122415b5137c80f6756b91f1144038f92c6fbe9
raghav <vindiesel38000@gmail.com>
2025-12-18 18:00:00 -0600

    feat(rotation): Integrate existing rotation lock logic

--- modules/rotation.js
+++ modules/rotation.js
@@ -1,30 +1,49 @@
 'use strict';
 
-const { GObject, St } = imports.gi;
+const { GObject, St, Gio } = imports.gi;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
 
 var RotationToggle = GObject.registerClass(
-class RotationToggle extends St.Bin {
+class RotationToggle extends PanelMenu.Button {
     _init(settings) {
-        super._init({
-            style_class: 'panel-button',
-            reactive: true,
-            can_focus: true,
-            track_hover: true
+        super._init(0.0, 'Rotation Lock');
+
+        this._appSettings = settings;
+        this.settings = new Gio.Settings({
+            schema_id: 'org.gnome.settings-daemon.peripherals.touchscreen'
         });
 
-        this._settings = settings;
-
-        let icon = new St.Icon({
-            icon_name: 'screen-rotation-symbolic',
+        this.icon = new St.Icon({
+            icon_name: 'rotation-locked-symbolic',
             style_class: 'system-status-icon'
         });
+        this.add_child(this.icon);
 
-        this.set_child(icon);
+        // Create toggle switch in menu
+        this.toggleItem = new PopupMenu.PopupSwitchMenuItem('Rotation Lock', false);
+        this.toggleItem.connect('toggled', this._onToggled.bind(this));
+        this.menu.addMenuItem(this.toggleItem);
 
-        this.connect('button-press-event', this._toggle.bind(this));
+        this._update();
+        this.settingsChanged = this.settings.connect('changed::orientation-lock', this._update.bind(this));
     }
 
-    _toggle() {
-        // Toggle logic goes here
+    _onToggled(item, state) {
+        this.settings.set_boolean('orientation-lock', state);
+    }
+
+    _update() {
+        let locked = this.settings.get_boolean('orientation-lock');
+        this.icon.icon_name = locked ? 'rotation-locked-symbolic' : 'rotation-allowed-symbolic';
+        this.toggleItem.setToggleState(locked);
+    }
+
+    destroy() {
+        if (this.settingsChanged) {
+            this.settings.disconnect(this.settingsChanged);
+        }
+        super.destroy();
     }
 });
